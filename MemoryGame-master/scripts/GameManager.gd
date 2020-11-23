0efb8b188c5211d156e15748732c9a997b85e009
theeramate1999 <6110110217@psu.ac.th>
2020-11-23 19:21:48 +0700

    first commit

---
+++ MemoryGame-master/scripts/GameManager.gd
@@ -0,0 +1,162 @@
+extends Node2D
+
+var start_time =preload("res://scenes/Counttime.gd")
+var card_face
+var card_back
+var init
+var number_of_matches
+var default_image
+var image1
+var image2
+var image3
+var image4
+var image5
+var image6
+var name_image1
+var name_image2
+var name_image3
+var name_image4
+var name_image5
+var name_image6
+var last_try_was_pair
+var card_one_checked_if_pairing
+var card_two_checked_if_pairing
+var card_one_string
+var card_two_string
+var all_remaining_cards = []
+var random_card
+var card_number
+
+
+func _ready():
+	number_of_matches = 0
+	card_one_string = "Card 1"
+	card_two_string = "Card 2"
+	last_try_was_pair = false
+	default_image = preload("res://sprites/dandelion-flower.png")
+	image1 = preload("res://sprites/ก.png")
+	name_image1 = "Kor Kai"
+	image2 = preload("res://sprites/ค.png")
+	name_image2 = "Buffalo "
+	image3 = preload("res://sprites/ข.png")
+	name_image3 = "Kor khai่"
+	image4 = preload("res://sprites/cycling.png")
+
+func _process(delta):
+	if !init:
+		_shuffle_cards()
+
+func _shuffle_cards():
+	all_remaining_cards = [1, 2, 3, 4, 5, 6]
+	
+	#prints (all_remaining_cards.size())
+	random_card = randi()% all_remaining_cards.size()
+	card_number = all_remaining_cards[random_card]
+	all_remaining_cards.remove(random_card)
+	var pair_1_card_1 = "Card" + String(card_number)
+	
+	random_card = randi()% all_remaining_cards.size()
+	card_number = all_remaining_cards[random_card]
+	all_remaining_cards.remove(random_card)
+	var pair_1_card_2 = "Card" + String(card_number)
+	
+	random_card = randi()% all_remaining_cards.size()
+	card_number = all_remaining_cards[random_card]
+	all_remaining_cards.remove(random_card)
+	var pair_2_card_1 = "Card" + String(card_number)
+	
+	random_card = randi()% all_remaining_cards.size()
+	card_number = all_remaining_cards[random_card]
+	all_remaining_cards.remove(random_card)
+	var pair_2_card_2 = "Card" + String(card_number)
+	
+	random_card = randi()% all_remaining_cards.size()
+	card_number = all_remaining_cards[random_card]
+	all_remaining_cards.remove(random_card)
+	var pair_3_card_1 = "Card" + String(card_number)
+	
+	
+	random_card = randi()% all_remaining_cards.size()
+	card_number = all_remaining_cards[random_card]
+	all_remaining_cards.remove(random_card)
+	var pair_3_card_2 = "Card" + String(card_number)
+	
+	
+	
+	
+	get_node(pair_1_card_1).card_face = image1
+	get_node(pair_1_card_1).card_name = name_image1
+	get_node(pair_1_card_2).card_face = image1
+	get_node(pair_1_card_2).card_name = name_image1
+	
+	get_node(pair_2_card_1).card_face = image2
+	get_node(pair_2_card_1).card_name = name_image2
+	get_node(pair_2_card_2).card_face = image2
+	get_node(pair_2_card_2).card_name = name_image2
+	
+	get_node(pair_3_card_1).card_face = image3
+	get_node(pair_3_card_1).card_name = name_image3
+	get_node(pair_3_card_2).card_face = image3
+	get_node(pair_3_card_2).card_name = name_image3
+	
+	
+	
+	
+	init = true
+
+func _check_if_pair():
+	if get_node("CardOneName").text == get_node("CardTwoName").text:
+		get_node("CheckBox").text = "="
+		last_try_was_pair = true
+		number_of_matches += 1
+		get_node("NumberOfMatches").text = "Number of Matches: " + String(number_of_matches)
+	if get_node("CardOneName").text != "Card 1" && get_node("CardTwoName").text != "Card 2":
+		if get_node("CardOneName").text != get_node("CardTwoName").text:
+			_disable_all_cards_clicks()
+			get_node("CheckBox").text = "!="
+			var waiting_timer = Timer.new()
+			waiting_timer.set_wait_time(2)
+			waiting_timer.set_one_shot(true)
+			self.add_child(waiting_timer)
+			waiting_timer.start()
+			yield(waiting_timer, "timeout")
+			_reset_card_name_strings_and_check_box()
+			_turn_around_cards()
+			_enable_all_cards_clicks()
+
+func _reset_card_name_strings_and_check_box():
+	get_node("CardOneName").text = card_one_string
+	get_node("CardTwoName").text = card_two_string
+	get_node("CheckBox").text = "?"
+
+func _turn_around_cards():
+	get_node(card_one_checked_if_pairing).get_node("Sprite").texture = default_image
+	get_node(card_two_checked_if_pairing).get_node("Sprite").texture = default_image
+
+func _disable_all_cards_clicks():
+	get_node("Card1").click_enabled = false
+	get_node("Card2").click_enabled = false
+	get_node("Card3").click_enabled = false
+	get_node("Card4").click_enabled = false
+	get_node("Card5").click_enabled = false
+	get_node("Card6").click_enabled = false
+
+
+func _enable_all_cards_clicks():
+	if get_node("Card1").get_node("Sprite").texture == default_image:
+		get_node("Card1").click_enabled = true
+	if get_node("Card2").get_node("Sprite").texture == default_image:
+		get_node("Card2").click_enabled = true
+	if get_node("Card3").get_node("Sprite").texture == default_image:
+		get_node("Card3").click_enabled = true
+	if get_node("Card4").get_node("Sprite").texture == default_image:
+		get_node("Card4").click_enabled = true
+	if get_node("Card5").get_node("Sprite").texture == default_image:
+		get_node("Card5").click_enabled = true
+	if get_node("Card6").get_node("Sprite").texture == default_image:
+		get_node("Card6").click_enabled = true
+	
+
+func _on_BackToMenuButton_button_down():
+	get_tree().reload_current_scene()
+	
